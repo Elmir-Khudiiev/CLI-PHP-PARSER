06a9364cd3c211b1152e21017b0e58bf3c7a9feb
Elmir Khudiiev <Elmir1993Elmir@gmail.com>
2019-01-28 14:51:50 +0200

    bug fix

--- classes/Links.php
+++ classes/Links.php
@@ -4,11 +4,10 @@ namespace classes;
 
 class Links
 {
-
     // Поиск ссылок.
     public static function listLinks($site, $url)
     {
-        preg_match_all('#<a.+?href=".+?//'.DOMAIN.'(.+?)".+?>#', $site, $links); // Ищем сылки только на указаном домене.
+        preg_match_all('#<a.+?href=".+?//' . DOMAIN . '(.+?)".+?>#', $site, $links); // Ищем сылки только на указаном домене.
         array_shift($links);
 
         foreach ($links[0] as $item) {
@@ -17,9 +16,8 @@ class Links
 
         $f_link = array_unique($f_link); // Проверка на дубливование.
 
-        array_unshift($f_link, "Source link: ".$url."\n", '');
+        array_unshift($f_link, "Source link: " . $url . "\n", '');
 
         return $f_link;
     }
-
 }
